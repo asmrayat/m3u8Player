89878c4db342579b80d2a40fc41a535046180087
asmrayat <asmrayat@gmail.com>
2023-06-10 12:31:52 +0600

    animation added

--- app/src/main/java/com/asmlab/m3u8player/MainActivity.java
+++ app/src/main/java/com/asmlab/m3u8player/MainActivity.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.view.Window;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
@@ -28,6 +29,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
 
         playBtn = findViewById(R.id.play);
         input = findViewById(R.id.inputText);
